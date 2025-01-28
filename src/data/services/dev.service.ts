369b787ee3a5ebf81993fc956f7c62757f36876d
Josadack <josadaqueferreira@outlook.com>
2025-01-28 10:38:57 -0300

    :sparkles: Configuções

---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'db_farmanet',
+          autoLoadEntities: true,
+          synchronize: true,
+          logging: true
+    };
+  }
+}
